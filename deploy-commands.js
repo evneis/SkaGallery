00b36723e5970b325fa986a2662661aa1c017f25
evneis <evanneiswender@gmail.com>
2025-05-21 23:09:33 -0400

    guild id changes

--- deploy-commands.js
+++ deploy-commands.js
@@ -34,25 +34,23 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 try {
   console.log(`Started refreshing ${commands.length} application (/) commands.`);
   
-  // Check if we have any guild IDs configured
-  if (botConfig.guildIds.length > 0) {
-    console.log(`Deploying commands to ${botConfig.guildIds.length} guilds...`);
+  // Check if we have a guild ID configured
+  if (process.env.GUILD_ID) {
+    console.log(`Deploying commands to guild ${process.env.GUILD_ID}...`);
     
-    // Deploy to each guild in the array
-    for (const guildId of botConfig.guildIds) {
-      const data = await rest.put(
-        Routes.applicationGuildCommands(
-          process.env.CLIENT_ID,
-          guildId
-        ),
-        { body: commands },
-      );
-      
-      console.log(`Successfully reloaded ${data.length} application (/) commands for guild ${guildId}.`);
-    }
+    // Deploy to the specified guild
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands },
+    );
+    
+    console.log(`Successfully reloaded ${data.length} application (/) commands for guild ${process.env.GUILD_ID}.`);
   } else {
-    // If no guild IDs specified, deploy globally
-    console.log('No guild IDs specified. Deploying commands globally (may take up to 1 hour to update).');
+    // If no guild ID specified, deploy globally
+    console.log('No guild ID specified. Deploying commands globally (may take up to 1 hour to update).');
     const data = await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands },
