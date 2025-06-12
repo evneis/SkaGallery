9fcc0e0ae54f683d5638e5ac297256b428971d4e
evneis <evanneiswender@gmail.com>
2025-06-12 19:59:07 -0400

    comments unneccessary

--- restart-skagallery.sh
+++ restart-skagallery.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Check if the process exists in PM2
 if pm2 list | grep -q "skagallery"; then
-    # If it exists, restart it
     /home/satanicpickle23/.nvm/versions/node/v22.16.0/bin/pm2 restart skagallery
 else
-    # If it doesn't exist, start it
     /home/satanicpickle23/.nvm/versions/node/v22.16.0/bin/pm2 start index.js --name "skagallery"
 fi
-
-# Log the action
 echo "SkaGallery $(if pm2 list | grep -q "skagallery"; then echo "restarted"; else echo "started"; fi) at $(date)" >> "$(dirname "$0")/pm2-restart.log" 
